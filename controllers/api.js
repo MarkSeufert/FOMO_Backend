1727bde1270dbe7842593ac335485e66272df3d5
Stefan Nestorovski <stefan.nestorovski@gmail.com>
2021-03-22 21:28:10 -0400

    Added image content blurring

--- controllers/api.js
+++ controllers/api.js
@@ -10,6 +10,10 @@ const commentController = require('./commentController')
 const postController = require('./postController')
 var Promise = require("bluebird");
 
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+var Jimp = require('jimp');
+
 // The credentials for the cloud storage will be found inside the computer's environment variables.
 // The cloud storage functionality won't work locally unless those credentials are set up. However,
 // it will work inside of the GCP instance.
@@ -81,31 +85,63 @@ router.post('/createPostWithImage', multer.single('image'), (req, res) => {
         res.status(400).send('No file uploaded.');
         return;
     }
+    
+    const img_buf = Buffer.from(req.file.buffer.toString('base64'), 'base64');
 
-    // Create a new blob in the bucket and upload the image data.
-    const imageName = req.file.fieldname + '-' + Date.now() + path.extname(req.file.originalname);
-    const blob = bucket.file(imageName);
-    const blobStream = blob.createWriteStream({
-        resumable: false,
-    });
-
-    blobStream.on('error', err => {
-        res.status(400).send('File upload failed.')
-    });
-
-    blobStream.on('finish', () => {
-        // The public URL can be used to directly access the file via HTTP.
-        const publicUrl = "https://storage.googleapis.com/" + bucket.name + "/" + blob.name;
-        console.log(`image uploaded to url: ${publicUrl}`);
-
-        let body = req.query;
-        body.imageUrl = publicUrl;
-        postController.createPostWithImage(body).then((posts) => {
-            res.send(posts);
+    const request = {
+        image: {
+            content: img_buf
+        }
+    };
+    
+    client.safeSearchDetection(request)
+    .then(([result]) => {
+        const safe = result.safeSearchAnnotation
+        if (safe.adult == 'LIKELY' || safe.adult == 'VERY_LIKELY' ||
+            safe.violence == 'LIKELY' || safe.violence == 'VERY_LIKELY') {
+            
+            console.log(`Detected image as inappropriate.`);
+            
+            return new Promise((resolve, reject) => {
+                Jimp.read(img_buf, (err, image) => {
+                    if (err) throw err;
+                    image.blur( 70 );
+                    req.file.buffer = image.bitmap.data;
+                    console.log("Image blurred");
+                    return resolve(req.file);
+                });
+            });
+        }
+        return Promise.resolve(req.file);
+    }).then(image => {
+        console.log("adding to cloud")
+        // Create a new blob in the bucket and upload the image data.
+        const imageName = image.fieldname + '-' + Date.now() + path.extname(image.originalname);
+        const blob = bucket.file(imageName);
+        const blobStream = blob.createWriteStream({
+            resumable: false,
         });
+    
+        blobStream.on('error', err => {
+            res.status(400).send('File upload failed.')
+        });
+    
+        blobStream.on('finish', () => {
+            // The public URL can be used to directly access the file via HTTP.
+            const publicUrl = "https://storage.googleapis.com/" + bucket.name + "/" + blob.name;
+            console.log(`image uploaded to url: ${publicUrl}`);
+    
+            let body = req.query;
+            body.imageUrl = publicUrl;
+            postController.createPostWithImage(body).then((posts) => {
+                res.send(posts);
+            });
+        });
+    
+        blobStream.end(image.buffer);
+    }).catch(err => {
+        console.error('ERROR:', err);
     });
-
-    blobStream.end(req.file.buffer);
 });
 
 module.exports = router;
